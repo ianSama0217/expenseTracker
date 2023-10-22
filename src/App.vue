01b02a9ff1cba4f3acba87a6fce68913f0ae370d
ianSama0217 <ian20000217@gmail.com>
2023-10-22 21:51:48 +0800

    complate main page typesetting

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,56 @@ import deletePopWindow from "./components/deletePopWindow.vue";
 </script>
 
 <template>
-  <!-- <sidebar />
-  <setAmountBox />
-  <addBtn />
-  <list /> -->
+  <div class="body">
+    <div class="leftBox">
+      <sidebar />
+    </div>
+
+    <div class="rightBox">
+      <div class="rightTopBox">
+        <setAmountBox />
+        <addBtn />
+      </div>
+      <div class="listArea">
+        <list />
+      </div>
+    </div>
+  </div>
 
   <!-- <addPopWindow /> -->
   <!-- <deletePopWindow /> -->
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.body {
+  display: flex;
+
+  .leftBox {
+    flex: 1 1 30vw;
+  }
+
+  .rightBox {
+    flex: 3 1 50vw;
+    display: flex;
+    flex-direction: column;
+    padding-top: 10vh;
+    padding-left: 10vw;
+
+    .rightTopBox {
+      height: 30vh;
+      width: 40vw;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .listArea {
+      height: 50vh;
+      width: 40vw;
+      margin-top: 1rem;
+      padding: 1rem;
+      overflow-y: scroll;
+    }
+  }
+}
+</style>
--- src/components/addBtn.vue
+++ src/components/addBtn.vue
@@ -6,12 +6,14 @@
 
 <style scoped lang="scss">
 button {
+  width: 14vw;
   background-color: #3c7fa5;
   color: white;
   border-radius: 8px;
   border: none;
   padding: 0.5rem 1rem;
   font-size: 1.25rem;
+  align-self: center;
 
   &:hover {
     cursor: pointer;
